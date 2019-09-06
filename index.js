1fa204eb2c170aa5f8e324ba0f6857f7a1d319ce
Omar Gonzalez <ogonzaleznyc@gmail.com>
2019-09-06 00:28:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Write your code in this file!
 
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
 const currentUser = 'Grace Hopper';
-
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
-const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+const welcomeMessage = 'Welcome to Flatbook, ';
